c82c640da54491958d4cd862074fc113902f3332
Akhil-peram <akhil01011@gmail.com>
2025-09-24 13:56:11 -0700

    Initializing project

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
-from bs4 import BeautifulSoup as bs
+# import libraries for scraping
+
+from bs4 import BeautifulSoup as bs  
 import requests as re
+
+# function for scraping
 
 def scraper(url):
     url = input("Enter Url of the site for Scraping : ")
 
     pass
-
